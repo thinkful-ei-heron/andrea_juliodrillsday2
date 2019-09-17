a5fadd3237450ba0b6b61facdaa1ec6b8269c032
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-17 11:25:28 -0500

    first commit

---
+++ drillsday2.js
@@ -0,0 +1,16 @@
+'use strict' 
+
+let numbers = [1,2,3,4];
+
+function max (numbers) 
+
+{ let i=0; while (numbers.length >= i) 
+    { if (numbers >= i) 
+        { return numbers; break; } 
+    };
+}
+console.log(max(numbers));
+
+function min (numbers) {
+
+};
