175a60a7a996c05a1a3d85c5565ce11fe4c9a6d1
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-17 13:35:24 -0500

    higher order function #1

--- drillsday2.js
+++ drillsday2.js
@@ -1,35 +1,47 @@
 'use strict' 
 
 function max(numbers) {
-    let maxNumber = numbers[0];
-    let i = 0;
-    while(i < numbers.length) {
-      if(numbers[i] > maxNumber) {
-        maxNumber = numbers[i];
-      }
-      i++;
+  let maxNumber = numbers[0];
+  let i = 0;
+  while(i < numbers.length) {
+    if(numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
     }
-   return maxNumber;
+    i++;
   }
+  return maxNumber;
+}
 
-  function min(numbers) {
-    let minNumber = numbers[0];
-    let i = 0;
-    while(i < numbers.length) {
-      if(numbers[i] < minNumber) {
-        minNumber = numbers[i];
-      }
-      i++;
+function min(numbers) {
+  let minNumber = numbers[0];
+  let i = 0;
+  while(i < numbers.length) {
+    if(numbers[i] < minNumber) {
+      minNumber = numbers[i];
     }
-    return minNumber;
+    i++;
   }
+  return minNumber;
+}
 
-  function average(numbers) {
-     let total = 0;
-     numbers.forEach(y => (total += y));
-    
-   let numAvg = total / numbers.length;
-   console.log(numAvg);
-  }
-   average([1,2,3,4,5]);
+function average(numbers) {
+  let total = 0;
+  numbers.forEach(y => (total += y));
+  let numAvg = total / numbers.length;
+  console.log(numAvg);
+}
+average([1,2,3,4,5]);
   
+function repeat(fn,n) {
+  for(let i = 0; i <= n; i++){
+    fn();
+  }
+}
+function hello(){
+  console.log('Hello Word');
+}
+function goodbye(){
+  console.log('Goodbye world');
+}
+
+console.log(repeat(hello,5));
